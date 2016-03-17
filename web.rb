8523e1a03212220c88940eb4d1a77d092ffa9c58
caffeinateddave <david.chaston@sky.uk>
2016-03-17 11:12:05 +0000

    Add env endpoint

--- web.rb
+++ web.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
+require 'dotenv'
+
+Dotenv.load
 
 get '/?' do
   "Hello, World!"
+end
+
+get '/env/?' do
+  out = ""
+  ENV.each do |k,v|
+    out += k + ": " + v + "<br/>"
+  end
+  out
 end
